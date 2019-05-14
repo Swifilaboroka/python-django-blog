7a6b1dbc73b75ce353990b238aedf7d02b74f851
Wembo Mulumba <fabriceotepa@hotmail.com>
2019-05-14 17:22:39 +0200

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+posts = [
+    {
+        'author' : 'Mark',
+        'title'  : 'Django Web Development',
+        'content': 'with Django so you can develop things rapidly, without having to deal with configuring a production server',
+        'date_posted' : 'Auguest 27, 2018'
+    },
+    {
+        'author' : 'Wembo Mulumba Otepa',
+        'title'  : 'Artificial Intelligence',
+        'content': 'with Django so you can develop things rapidly, without having to deal with configuring a production server',
+        'date_posted' : 'December 21, 2018'
+    },
+
+    {
+        'author' : 'Wembo Mulumba Otepa',
+        'title'  : 'C++ Programming',
+        'content': 'with Django so you can develop things rapidly, without having to deal with configuring a production server',
+        'date_posted' : 'December 28, 2018'
+    }
+]
+
+def home(request):
+    #return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title' : 'About'})
+
+def contact(request):
+    return HttpResponse('<h1>Contact Blog Author</h1>')    
+
+#blog -> templates -> blog -> template.html

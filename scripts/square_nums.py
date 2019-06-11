f7c9e7fe99cfd712126dfe67a51009d83071de8a
Wembo Mulumba <fabriceotepa@hotmail.com>
2019-06-11 16:35:44 +0200

    Create square_nums.py

---
+++ scripts/square_nums.py
@@ -0,0 +1,9 @@
+def square_numbers(nums):
+    result = []
+    for i in nums:
+        result.append(i*i)
+    return result
+
+my_nums = square_numbers([487000000,20000,300254,40,55598777])
+
+print (my_nums)
